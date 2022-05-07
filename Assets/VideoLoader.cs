f7985d7c9c231110636d81df13f556dabf1eb118
hassy_mxk <mxk02371@gmail.com>
2022-05-07 16:59:06 +0900

    test movie

---
+++ Assets/VideoLoader.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Video;
+[RequireComponent(typeof(VideoPlayer))]
+public class VideoLoader : MonoBehaviour
+{
+    [SerializeField]
+    private string relativePath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VideoPlayer player = GetComponent<VideoPlayer>();
+        player.source = VideoSource.Url;
+        player.url = Application.streamingAssetsPath + "/" + relativePath;
+        player.prepareCompleted += PrepareCompleted;
+        player.Prepare();
+    }
+    void PrepareCompleted(VideoPlayer vp)
+    {
+        vp.prepareCompleted -= PrepareCompleted;
+        vp.Play();
+    }
+}
